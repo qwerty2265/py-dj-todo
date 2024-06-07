4b9c42ed57d4ef847153d48655e832af879428bb
Maks <maks_mun@bk.ru>
2024-06-07 20:15:27 +0500

    Create category model

--- todo/models.py
+++ todo/models.py
@@ -4,3 +4,7 @@ class Task(models.Model):
     title = models.TextField()
     description = models.TextField()
     completed = models.BooleanField(default=False)
+    category = models.ForeignKey(to='Category', to_field='name', on_delete=models.CASCADE)
+
+class Category(models.Model):
+    name = models.TextField();
