37eb75cb3735a4413cf60c9a7b79d0670963d51f
Maks <maks_mun@bk.ru>
2024-06-07 20:41:20 +0500

    Create CRUD for category model

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,10 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Task
+from .models import Task, Category
 
 class TaskListView(ListView):
     model = Task
     template_name = 'task_list.html'
-
+    
 class TaskDetailView(DetailView):
     model = Task
     template_name = 'task_detail.html'
@@ -23,3 +23,22 @@ class TaskDeleteView(DeleteView):
     model = Task
     success_url = '/'
     template_name = 'task_form.html'
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category_list.html'
+
+class CategoryCreateView(CreateView):
+    model = Category
+    fields = ['name']
+    template_name = 'category_form.html'
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    fields = ['name']
+    template_name = 'category_form.html'
+
+class CategoryDeleteView(DeleteView):
+    model = Category
+    success_url = '/'
+    template_name = 'category_form.html'
