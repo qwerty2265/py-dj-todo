533788c38cb2ab372cf646c78fba221f897a39aa
Maks <maks_mun@bk.ru>
2024-06-07 20:37:01 +0500

    Add paths for views

--- todo/urls.py
+++ todo/urls.py
@@ -1,22 +1,17 @@
-"""
-URL configuration for todo project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from .views import (
+    TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView,
+    CategoryListView, CategoryCreateView, CategoryUpdateView, CategoryDeleteView
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', TaskListView.as_view(), name='task_list'),
+    path('task/<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
+    path('task/new/', TaskCreateView.as_view(), name='task_create'),
+    path('task/<int:pk>/edit/', TaskUpdateView.as_view(), name='task_edit'),
+    path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
+    path('categories/', CategoryListView.as_view(), name='category_list'),
+    path('category/new/', CategoryCreateView.as_view(), name='category_create'),
+    path('category/<int:pk>/edit/', CategoryUpdateView.as_view(), name='category_edit'),
+    path('category/<int:pk>/delete/', CategoryDeleteView.as_view(), name='category_delete'),
 ]
